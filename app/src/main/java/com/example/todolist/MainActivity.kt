d31ab3739957e941ab57074bd084ae27a021814d
ahervoue <arnaud.hervouet@laposte.net>
2023-09-11 11:29:34 +0200

    Ajout d'éléments dans listView et création layout dialog

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnNewToDoList = findViewById<Button>(R.id.btnNewToDoList)
+        val listToDo = findViewById<ListView>(R.id.listToDoList)
+        var listTest = arrayOf("Test1", "Test2", "Test3")
+        var adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listTest)
+
+        listToDo.adapter = adapter
+
+        btnNewToDoList.setOnClickListener {
+            listTest += "Test4"
+            adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listTest)
+            listToDo.adapter = adapter
+        }
     }
 }
