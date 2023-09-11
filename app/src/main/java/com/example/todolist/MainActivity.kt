6d437083da2047a1e6a97474fc38ecb89d560e6c
ahervoue <arnaud.hervouet@laposte.net>
2023-09-11 13:56:48 +0200

    Ajout de la fonctionnalité d'ajout de liste de tâches

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.example.todolist
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
-import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.ListView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnNewToDoList = findViewById<Button>(R.id.btnNewToDoList)
+        val texteNomListe = findViewById<EditText>(R.id.editTextText)
+        val btnNewToDoList = findViewById<ImageButton>(R.id.imgBtnListe)
         val listToDo = findViewById<ListView>(R.id.listToDoList)
         var listTest = arrayOf("Test1", "Test2", "Test3")
         var adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listTest)
@@ -19,9 +23,17 @@ class MainActivity : AppCompatActivity() {
         listToDo.adapter = adapter
 
         btnNewToDoList.setOnClickListener {
-            listTest += "Test4"
-            adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listTest)
-            listToDo.adapter = adapter
+            if(texteNomListe.text.toString() != "") {
+                listTest += texteNomListe.text.toString()
+                adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listTest)
+                listToDo.adapter = adapter
+                texteNomListe.text.clear()
+            }
+            else{
+                val toast = Toast.makeText(this, "Vous devez renseigner un nom de liste !", Toast.LENGTH_SHORT) // in Activity
+                toast.show()
+            }
         }
     }
+
 }
